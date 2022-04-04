088026c2da115fd68b96264002fc59ec0e31914c
choaib moncef <choaibmoncef1@gmail.com>
2022-04-04 01:12:31 +0000

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+// import { io } from "socket.io-client";
+import socketService from "./services/socketService";
+import { JoinRoom } from "./components/joinRoom";
+import GameContext, { IGameContextProps } from "./gameContext";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em;
+`;
+
+const WelcomeText = styled.h1`
+  margin: 0;
+  color: #8e44ad;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
+
+  const [isInRoom, setInRoom] = useState(false)
+  const [playerSymbol, setPlayerSymbol] = useState<"x" | "o">("x")
+  const [isPlayerTurn, setPlayerTurn] = useState(false)
+  const [isGameStarted, setGameStarted] = useState(false)
+
+  const connectSocket = async () => {
+    const socket = socketService
+      .connect("http://localhost:9000")
+      .catch((err) => {
+        console.error("Error: ", err);
+      });
+  };
+
+  useEffect(() => {
+    connectSocket();
+  }, [])
+
+  const gameContextValue: IGameContextProps = {
+    isInRoom,
+    setInRoom,
+    playerSymbol,
+    setPlayerSymbol,
+    isPlayerTurn,
+    setPlayerTurn,
+    isGameStarted,
+    setGameStarted
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GameContext.Provider value={gameContextValue}>
+    <AppContainer>
+      <WelcomeText>Welcome to tictactoe</WelcomeText>
+      <MainContainer>
+        <JoinRoom />
+      </MainContainer>
+    </AppContainer>
+    </GameContext.Provider>
   );
 }
 
